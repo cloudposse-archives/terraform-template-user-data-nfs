38da0762bf63083cb7945bb1da96034d3ae0a4fb
Igor Rodionov <goruha@gmail.com>
2017-08-02 18:45:06 +0600

    Added nfs mount user data

--- main.tf
+++ main.tf
@@ -16,28 +16,3 @@ data "template_file" "default" {
     host       = "${var.host}"
   }
 }
-
-## IAM Role Policy that allows access to S3
-resource "aws_iam_policy" "default" {
-  name = "${null_resource.default.triggers.id}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  policy = "${data.aws_iam_policy_document.default.json}"
-}
-
-data "aws_iam_policy_document" "default" {
-  statement {
-    actions = [
-      "ec2:DescribeInstances",
-    ]
-
-    effect = "Allow"
-
-    resources = [
-      "*",
-    ]
-  }
-}
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "user_data" {
   value = "${data.template_file.default.rendered}"
 }
-
-output "policy_arn" {
-  value = "${aws_iam_policy.default.arn}"
-}
