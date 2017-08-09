e18dcfac07c984ee3d219e81461f77da50e96966
Igor Rodionov <goruha@gmail.com>
2017-08-09 20:38:57 +0600

    Address PR comments

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
-resource "null_resource" "default" {
-  triggers = {
-    id = "${lower(format("%v-%v-%v", var.namespace, var.stage, var.name))}"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+# Define composite variables for resources
+module "label" {
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.1.0"
+  namespace = "${var.namespace}"
+  name      = "${var.name}"
+  stage     = "${var.stage}"
 }
 
 data "template_file" "default" {
