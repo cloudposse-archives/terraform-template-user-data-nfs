04827b75cfaa4b3d7788efdf25d3f42fc586dcd1
Igor Rodionov <goruha@gmail.com>
2017-08-10 00:17:29 +0600

    Define User Data for Mounting EFS/NFS Volume (#1)
    * Added nfs mount user data
    
    * Added nfs mount user data
    
    * Address PR comments

---
+++ main.tf
@@ -0,0 +1,16 @@
+# Define composite variables for resources
+module "label" {
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.1.0"
+  namespace = "${var.namespace}"
+  name      = "${var.name}"
+  stage     = "${var.stage}"
+}
+
+data "template_file" "default" {
+  template = "${file("${path.module}/user_data.sh")}"
+
+  vars {
+    dir       = "${var.dir}"
+    host       = "${var.host}"
+  }
+}
