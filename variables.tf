b2d0efd932f06e2f26de2d15d0b55a8c4dbd669b
vadim-hleif <vadzim.hleif@gmail.com>
2018-07-18 16:59:31 +0300

    Migrate readme yaml

--- variables.tf
+++ variables.tf
@@ -1,30 +1,31 @@
+variable "name" {
+  description = "The Name of the application or solution  (e.g. `bastion` or `portal`)"
+}
+
 variable "namespace" {
-  description = ""
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
 }
 
 variable "stage" {
-  description = ""
-}
-
-variable "name" {
-  description = ""
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "delimiter" {
-  description = ""
+  type        = "string"
   default     = "-"
+  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
 }
 
 variable "attributes" {
-  description = ""
   type        = "list"
   default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
 }
 
 variable "tags" {
-  description = ""
   type        = "map"
   default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`)"
 }
 
 variable "dir" {
